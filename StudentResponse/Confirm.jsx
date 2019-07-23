7b022650be404b1d60eb08dc2c74eab28d2d9b02
Elizabeth Ellis <Elizabeth.Ellis@americanfidelity.com>
2019-07-23 14:32:05 -0500

    Modal attempt

--- StudentResponse/Confirm.jsx
+++ StudentResponse/Confirm.jsx
@@ -1,15 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-function Confirm() {
-  return (
-    <div>
-      <p>Thanks for your input!</p>
-      <img src="LikeButton.png" alt="LikeButton" />
-      {/* <ConfirmDetails Questions={"Hi there"}/> */}
+<div class="modal" tabindex="-1" role="dialog">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Thanks for your input!</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <img src="LikeButton.png" alt="LikeButton" />
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
     </div>
-  );
-}
+  </div>
+</div>
+
+
+// function Confirm() {
+//   return (
+//     <div>
+//       <p>Thanks for your input!</p>
+//       <img src="LikeButton.png" alt="LikeButton" />
+//       {/* <ConfirmDetails Questions={"Hi there"}/> */}
+//     </div>
+//   );
+// }
 
 // function ConfirmDetails({Questions}) {
 //   return (
