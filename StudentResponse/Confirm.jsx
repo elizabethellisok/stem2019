3282870505cb1aab35be3a101ec7ee7300e4dc1a
Elizabeth Ellis <Elizabeth.Ellis@americanfidelity.com>
2019-07-24 14:57:06 -0500

    Pete started working on streamlining the page

--- StudentResponse/Confirm.jsx
+++ StudentResponse/Confirm.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+/*
 <div class="modal" tabindex="-1" role="dialog">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title">Thanks for your input!</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+        <button type="button" class="close" data-dismiss="mpodal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
@@ -19,17 +19,17 @@ import { Link } from "react-router-dom";
     </div>
   </div>
 </div>
+*/
 
-
-// function Confirm() {
-//   return (
-//     <div>
-//       <p>Thanks for your input!</p>
-//       <img src="LikeButton.png" alt="LikeButton" />
-//       {/* <ConfirmDetails Questions={"Hi there"}/> */}
-//     </div>
-//   );
-// }
+function Confirm() {
+   return (
+     <div>
+      <p>Thanks for your input!</p>
+       <img src="LikeButton.png" alt="LikeButton" />
+       {/* <ConfirmDetails Questions={"Hi there"}/> */}
+     </div>
+   );
+ }
 
 // function ConfirmDetails({Questions}) {
 //   return (
--- StudentResponse/SelectClass.jsx
+++ StudentResponse/SelectClass.jsx
@@ -1,35 +1,78 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+var hourChosen;
+var studentChosen;
+var questionChosen;
+
 function SelectClass() {
   return (
     <div>
-      <p>Select your class:</p>
-      <Dropdown classes={["1st Hour", "2nd Hour", "3rd Hour"]}></Dropdown>
-      <ClassList Classes={["1st Hour", "2nd Hour", "3rd Hour"]}></ClassList>
+      <SelectHour hours={["1st Hour", "2nd Hour", "3rd Hour"]}></SelectHour>
+      <br></br>
+      <SelectStudent students={["Fred", "Ted"]}></SelectStudent>
+      <br></br>
+      <SelectQuestion questions={["How do you feel?", "How do you feel about the lesson?"]}></SelectQuestion>
     </div>
-  )
+  );
 }
 
-function Dropdown({ classes }) {
+function SelectHour({ hours }) {
   return (
     <div class="dropdown">
-      <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+      <button class="btn btn-secondary dropdown-toggle" type="button" id="classDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
         Select Class
       </button>
       <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-        {classes.map(hour => {
-          return <a class="dropdown-item" href={"#"+ hour}>{hour}</a>
+        {hours.map(hour => {
+          return <a class="dropdown-item" onClick={HourSelected(hour)}>{hour}</a>
         })}
       </div>
     </div>
   );
 }
 
-function ClassList({ Classes }) {
+function SelectStudent({ students }) {
+    if(hourChosen == null)
+      return null;
+
+   return (
+     
+    <div class="dropdown">
+      <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        Select Student
+      </button>
+      <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+        {students.map(student => {
+          return <a class="dropdown-item" href={"#"+ student}>{student}</a>
+        })}
+      </div>
+    </div>
+   );
+}
+
+function SelectQuestion({ questions }) {
+  if(hourChosen == null)
+      return null;
+
   return (
-    <div>{Classes}</div>
-  )
+    <div class="dropdown">
+      <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        Select Question
+      </button>
+      <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+        {questions.map(question => {
+          return <a class="dropdown-item" href={"#"+ question}>{question}</a>
+        })}
+      </div>
+    </div>
+   );
+}
+
+function HourSelected({ hour }) {
+  hourChosen = hour;
+  
+  return null;
 }
 
 export default SelectClass;
